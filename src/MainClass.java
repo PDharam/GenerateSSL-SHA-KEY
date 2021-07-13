e3be5ad287885011a7d718dc318e620239c833da
Pravin Dharam <dharam.pravin@mahindra.com>
2021-07-13 14:03:11 +0530

    #Add comment for understanding

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,6 +2,7 @@ import java.io.File;
 
 public class MainClass {
     public static void main(String[] args) {
+        //Here you can put file format such as .crt, .cer, .der or .pem.
         File certificate = new File("C:\\Users\\DELL\\Downloads\\mahindradealerworld-com-chain.pem");
         String peerCertificate = PeerCertificateExtractor.extract(certificate);
         System.out.println("SSL CERT KEY: " + peerCertificate);
